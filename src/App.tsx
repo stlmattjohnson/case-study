fa7ffa37ba09bb2cf377726f005c3d9559972470
Matt Johnson <stlmattjohnson@outlook.com>
2022-02-12 01:06:03 -0500

    build: add additional dependencies
    Added several new dependencies: chakra-ui, react-router-dom, react-query
    
    Configured each and added necessary handling to App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from "react";
-import "./App.css";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ChakraProvider } from "@chakra-ui/react";
 
-function App() {
+const queryClient = new QueryClient();
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header" data-testid="App-header">
-        Case Study
-      </header>
-    </div>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider>
+          <div className="App">
+            <header className="App-header" data-testid="App-header">
+              Case Study
+            </header>
+          </div>
+        </ChakraProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
